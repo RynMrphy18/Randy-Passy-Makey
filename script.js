b595778dc8aa9a9f08610e6622f385fe0859b617
Ryan <rmurph1800@gmail.com>
2021-08-15 15:41:13 -0400

    added global variables/arrays

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 // Assignment code here
-var
+
+//defined global variables
+var enter;
+var confirmNumber;
+var confirmSpecial;
+var confirmUpper;
+var confirmLower;
+
+// input arrays (numbers, special characters, uppercase letters, lowercase letters)
+number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ":", ";", "'", "<", ">", ",", ".", "?", "/"];
+
+upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
