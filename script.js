aa5894a379bf05de544aa0e060c74de375916fac
Ryan <rmurph1800@gmail.com>
2021-08-15 16:07:16 -0400

    added first conditionals

--- script.js
+++ script.js
@@ -21,14 +21,42 @@ lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// event listener linked to button in content section 
+generateBtn.addEventListener("click", function(){
+    ps = writePassword();
+    document.getElementById("#password").placeholder = ps;
+});
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+enter = parseInt(prompt("How many characters would you like in your password? Please choose between 8 and 128."));
+
+// first criteria alert
+if (!enter) { 
+  window.alert("Please enter a value!");
+}
+
+// second criteria alert/prompt
+else if (enter < 8 || enter > 128) {
+  enter = parseInt(prompt("Inputted value not between 8 and 128."));
+}
+
+// user criteria input
+else {
+  confirmNumber = window.confirm("Should this contain numbers?");
+  confirmSpecial = window.confirm("Should this contain special characters?");
+  confirmUpper = window.confirm("Should this contain uppercase letters?");
+  confirmLower = window.confirm("Should this contain lowercase letters?");
+};
